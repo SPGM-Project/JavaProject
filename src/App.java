39b3c7f82262e4eadc8f5c53ada8619a54d1ca09
Seungjee Lee <moondgod@gmail.com>
2022-11-08 16:22:25 +0900

    Add stack

--- src/App.java
+++ src/App.java
@@ -1,23 +1,40 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Stack s = new Stack(10);
-        s.init();
-        s.push(1);
-        s.push(2);
-        s.push(3);
-        s.push(4);
-        s.push(5);
-        s.peek();
-        s.pop();
-        s.pop();
-        s.pop();
-        s.pop();
+        Queue q = new Queue(10);
+        q.init();
+        q.printAll();
+        q.enQueue(1);
+        q.enQueue(2);
+        q.enQueue(3);
+        q.enQueue(4);
+        q.enQueue(5);
+        q.enQueue(6);
+        q.enQueue(7);
+        q.enQueue(8);
+        q.printAll();
+        q.deQueue();
+        q.deQueue();
+        q.deQueue();
+        q.deQueue();
+        q.printAll();
+        q.enQueuedj(9);
+        q.enQueue(10);
+        q.printAll();
+        q.deQueue();
+        q.deQueue();
+        q.deQueue();
+        q.deQueue();
+        q.enQueue(1);
+        q.enQueue(2);
+        q.enQueue(3);
+        q.printAll();
     }
 }
 class ArrayList{
     int[] List;
     int numOfDatas;
     int List_Size;
+
     ArrayList(int List_Size){
         this.List_Size = List_Size;
         this.List = new int[List_Size];
@@ -84,7 +101,7 @@ class Stack{
     int[] Stack;
     int topIdx;
     int Stack_Size;
-    Stack(int Stack_Size){
+    Stack(int Stack_Size) {
         this.Stack_Size = Stack_Size;
         this.Stack = new int[Stack_Size];
         this.topIdx = -1;
@@ -110,5 +127,53 @@ class Stack{
 
     void peek() {
         System.out.println("가장 위의 값 : " + Stack[topIdx]); 
+    }
+}
+
+class Queue{
+    int[] Queue;
+    int front;
+    int rear;
+    int Queue_Size;
+
+    Queue(int Queue_Size){
+        this.Queue_Size = Queue_Size;
+        this.Queue = new int[Queue_Size];
+        this.front = 0;
+        this.rear = 0;
+    }
+
+    void init() {
+        front = 0;
+        rear = 0;
+    }
+
+    void enQueue(int insertData) {
+        if((rear+1)%Queue_Size == front){
+            System.out.println("꽉찼습니다!");
+            return;
+        }
+        rear = (rear+1)%Queue_Size;
+        Queue[rear] = insertData;
+    }
+
+    void deQueue() {
+        if(front == rear){
+            System.out.println("지울 데이터가 없습니다");
+            return;
+        }
+        front = (front+1)%Queue_Size;
+        System.out.println(Queue[front] + " 삭제함");
+    }
+
+    void printAll() {
+        if(front == rear){
+            System.out.println("데이터가 없습니다");
+            return;
+        }
+        for(int i = front; (i % Queue_Size) != rear; i++ ){
+            System.out.print("[" + Queue[(i+1)%Queue_Size] + "]");
+        }
+        System.out.println();
     }
 }
