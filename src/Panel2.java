7e6d51ac589e9584bae4ffe399736d3b084765d6
Seungjee Lee <moondgod@gmail.com>
2022-11-21 14:38:56 +0900

    basic

--- src/Panel2.java
+++ src/Panel2.java
@@ -9,26 +9,13 @@ import javax.swing.JTextField;
 
 public class Panel2 extends JPanel {
     Panel2() {
-        setLayout(new BorderLayout());
         JPanel upperPanel = new JPanel();
-        JPanel middlePanel = new JPanel();
-        JPanel lowerPanel = new JPanel();
-        
+        upperPanel.add(new JLabel("LinkedList"));
         upperPanel.add(new JTextField(10));
-        upperPanel.add(new JLabel("월"));
-        upperPanel.add(new JTextField(10));
-        upperPanel.add(new JLabel("일"));
-        
-        add(upperPanel, BorderLayout.NORTH);        
-        
-        JTextArea textArea = new JTextArea(10, 30);
-        JScrollPane scrollPane = new JScrollPane(textArea);
-        middlePanel.add(scrollPane);
-        
-        add(middlePanel, BorderLayout.CENTER);
-        
-        lowerPanel.add(new JButton("저장"));
-        add(lowerPanel, BorderLayout.SOUTH);
+        upperPanel.add(new JLabel("정수값 입력"));
+        add(upperPanel, BorderLayout.NORTH);
+        add(new JButton("Node추가"));
+        add(new JButton("Node추가"));
         
         setVisible(false);
     }
