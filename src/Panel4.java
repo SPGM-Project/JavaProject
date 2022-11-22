3b47c09720b162ddbce7826d94d4db691856a28b
LeeKiHwan <s22049@gsm.hs.kr>
2022-11-23 07:37:30 +0900

    queue

--- src/Panel4.java
+++ src/Panel4.java
@@ -1,21 +1,63 @@
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-public class Panel4 extends JPanel {
+public class Panel4 extends JPanel implements ActionListener {
+    JButton enQueueBtn = new JButton("EnQueue");
+    JButton queueInitBtn = new JButton("Queue Init");
+    JButton deQueueBtn = new JButton("DeQueue");
+
+    JTextField textField = new JTextField(10);
+
+    JPanel upperPanel = new JPanel();
+    JPanel middlePanel = new JPanel();
+
+    JLabel queueLabel = new JLabel();
+
+    JOptionPane aa = new JOptionPane();
+	String number = aa.showInputDialog("Queue의 Size 입력");
+    int num = Integer.valueOf(number);
+    Queue queue = new Queue(num);
+    
     Panel4() {
-        JPanel upperPanel = new JPanel();
+        setLayout(new BorderLayout());
+
         upperPanel.add(new JLabel("Queue"));
-        upperPanel.add(new JTextField(10));
+        upperPanel.add(textField);
         upperPanel.add(new JLabel("정수값 입력"));
-        add(upperPanel, BorderLayout.NORTH);        
-        add(new JButton("등록"));
+        upperPanel.add(enQueueBtn);
+        upperPanel.add(deQueueBtn);
+        upperPanel.add(queueInitBtn);
+        add(upperPanel, BorderLayout.NORTH);
+        
+        middlePanel.add(queueLabel);
+        add(middlePanel, BorderLayout.CENTER);
+
+        enQueueBtn.addActionListener(this);
+        deQueueBtn.addActionListener(this);
+        queueInitBtn.addActionListener(this);
         
         setVisible(false);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        if (e.getSource() == enQueueBtn) {
+            queue.enQueue(Integer.parseInt(textField.getText()));
+            queueLabel.setText("");
+        }
+        else if (e.getSource() == deQueueBtn) {
+
+        }
+        else if (e.getSource() == queueInitBtn) {
+
+        }
     }
 }
